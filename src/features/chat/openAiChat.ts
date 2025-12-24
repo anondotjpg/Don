b8b220daa300d9552ba77c967aae61b7594f58c4
agorapilled <johngardner13@outlook.com>
2025-12-24 06:35:14 -0500

    v0

--- src/features/chat/openAiChat.ts
+++ src/features/chat/openAiChat.ts
@@ -48,7 +48,7 @@ export async function getChatResponseStream(
       try {
 
         const OPENROUTER_API_KEY = openRouterKey;
-        const YOUR_SITE_URL = 'https://www.waifu-gamma.vercel.app/';
+        const YOUR_SITE_URL = 'https://pumpsan-u9wx.vercel.app/';
         const YOUR_SITE_NAME = 'ChatVRM';
 
         let isStreamed = false;
